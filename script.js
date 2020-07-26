1532acff04a731d166053793ca6ca4bf0d1c0f9b
Marina Moore <mnmoore1112@gmail.com>
2020-07-26 16:38:11 -0500

    updates to submit

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-// Write your JavaScript code here!
-
-<script>
+// checking the form field
    window.addEventListener("load", function() {
-      let form = document.querySelector("launchForm");
+      let form = document.querySelector("#launchForm");
       form.addEventListener("submit", function(event) {
          let nameInput = document.querySelector("input[name=pilotName]");
          let copilotInput = document.querySelector("input[name=copilotName]");
-         let fuelInput = document.querySelector("input[liters=fuelLevel]");
-         let cargoMassInput = document.querySelector("input[kilograms=cargoMass]")
+         let fuelInput = document.querySelector("input[name=fuelLevel]");
+         let fuelInputToNum = Number(fuelInput);
+         let cargoMassInput = document.querySelector("input[name=cargoMass]");
+         let cargoMassToNum = Number(cargoMassInput);
          if (nameInput.value === "" || copilotInput.value === "" || fuelInput.value === "" || cargoMassInput.value === "") {
             alert("All fields are required!");
             // stop the form submission
@@ -17,12 +17,35 @@
             alert("Please enter numeric values only.");
             event.preventDefault();
          }
-         if (typeof(nameInput.value) != "string" || typeof(copilotInput.value) != "string" || typeof(fuelInput.value) != "number" || typeof(cargoMassInput) != "number") {
-            alert("Invalid response. Name field must only contain alphabetic characters. Fuel level and cargo mass must contain numeric characters only.")
+         if (typeof(nameInput.value) !== "string" || typeof(copilotInput.value) !== "string" || typeof(fuelInputToNum) !== "number" || typeof(cargoMassToNum) !== "number") {
+            alert("Invalid response. Name field must only contain alphabetic characters. Fuel level and cargo mass must contain numeric characters only.");
+            event.preventDefault();
+         }
+      });
+ //checking faulty items
+      let statusCheck = document.querySelector("#launchStatusCheck");
+      statusCheck.addEventListener("submit", function(event) {
+         document.getElementById("pilotStatus").innerHTML = "${nameInput.value} is ready.";
+         document.getElementById("copilotStatus").innerHTML = "${copilotInput.value} is ready.";
+         if (fuelInputToNum < 10000) {
+            document.getElementById("faultyItems").style.display = "visible";
+            document.getElementById("fuelStatus").innerHTML = "Not enough fuel for journey.";
+            document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch.";
+            document.getElementById("launchStatus").style.color = "red";
+         }
+         if (cargoMassToNum > 10000) {
+            document.getElementById("faultyItems").style.display = "visible";
+            document.getElementById("cargoStatus").innerHTML = "Cargo mass too high for take off.";
+            document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch.";
+            document.getElementById("launchStatus").style.color = "red";
+         }
+         else {
+            document.getElementById("launchStatus").innerHTML = "Shuttle is ready for launch.";
+            document.getElementById("launchStatus").style.color = "green"; 
          }
       });
    });
-</script>
+
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
